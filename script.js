2ba1a027b1e4ee6181a6d8417c738627489ef28c
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-07 00:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
 
-	let s1lower = s1.toLoweCase();
+	let s1lower = s1.toLowerCase();
 	let s2lower = s2.toLowerCase();
 	return s1lower.indexOf(s2lower);
 }
